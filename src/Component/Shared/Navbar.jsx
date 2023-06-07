0217a92ac655775090636888feb14a149c4d1750
sadiawcutecat <98077710+sadiawcutecat@users.noreply.github.com>
2023-06-07 11:49:54 +0600

    navbar modified

--- src/Component/Shared/Navbar.jsx
+++ src/Component/Shared/Navbar.jsx
@@ -1,34 +1,35 @@
 import { Link } from "react-router-dom";
+import logo from '../../assets/images/navLogo.png'
 
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-accent font-bold px-4">
+        <div className="navbar bg-White shadow-md font-bold px-4 ">
             <div className="navbar-start">
-                <img className="h-9 w-9" src="" alt="" />
-                <a className="text-2xl text-white ms-4">Foreign Language <br /> Learning School</a>
+                <img className="h-12 w-12" src={logo} alt="" />
+                <a className="text-2xl ms-4">EduLine</a>
             </div>
             <div className="navbar-center  lg:flex">
-                <ul className="menu menu-horizontal px-1 text-white">
+                <ul className="menu menu-horizontal px-1">
                     <Link to='/'><li><a>Home</a></li></Link>
-                    <Link to='/AllToys'><li><a>Instructors</a></li></Link>
-                    <Link to='/myToys'> <li><a>Classes</a></li></Link>
-                    <Link to='/AddToys'><li><a>Dashboard</a></li></Link>
-
-
-                    <Link to='/blogs'><li><a>Blogs</a></li></Link>
+                    <Link to='/'><li><a>Instructors</a></li></Link>
+                    <Link to='/'> <li><a>Classes</a></li></Link>
+                    <Link to='/'><li><a>Dashboard</a></li></Link>
                 </ul>
             </div>
             <div className="navbar-end gap-2">
+            <FaUserCircle style={{ fontSize: '2rem' }} className="text-white" />
+            <button className="btn btn-outline btn-primary text-opacity-70 font-bold px-5">Sign in</button>
+            <button className="btn  btn-primary bg-opacity-70 text-white font-bold px-5">Sign up</button>
                 {/* <FaUserCircle style={{ fontSize: '2rem' }} className="text-white" /> */}
 
-                <Link to='/signout'>
-                    <button className="text-white py-2 px-5 border border-white">Sing Out</button>
+                {/* <Link to='/signout'>
+                    <button className= " py-2 px-5 border border-white">Sing Out</button>
                 </Link>
 
                 <Link to='/login'>
-                    <button className="text-white py-2 px-5 border border-white">login</button>
-                </Link>
+                    <button className=" py-2 px-5 border border-white">login</button>
+                </Link> */}
 
 
 
